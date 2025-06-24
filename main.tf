5af36b7caa3607d1a1d9fd085fc8f7c25d5bf8ea
spartan-haopham <hao.pham@c0x12c.com>
2025-06-24 14:35:07 +0700

    update

--- main.tf
+++ main.tf
@@ -122,7 +122,7 @@ data "aws_iam_policy_document" "this" {
       sid       = lookup(statement.value, "sid", "CustomStatement")
       effect    = lookup(statement.value, "effect", "Allow")
       actions   = lookup(statement.value, "actions", [])
-      resources = statement.value.resources != null ? statement.value.resources : [local.bucket.arn, "${local.bucket.arn}/*"]
+      resources = coalesce(statement.value.resources, [local.bucket.arn, "${local.bucket.arn}/*"])
 
       dynamic "principals" {
         for_each = lookup(statement.value, "principals", null) != null ? [statement.value.principals] : []
